a83ddc81fe02fe3f8c1d452ab65ffd862c15e881
biteOver3ide <youssefmatthiesen@gmail.com>
2021-11-10 10:41:29 +0100

    Program changed

--- GitHub_test/Program.cs
+++ GitHub_test/Program.cs
@@ -9,6 +9,21 @@ namespace GitHub_test
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!" +(new you()).Add(7,7));
+            
+
+
+            int[] github = { 12, 36, 352, 36352, 32541, -653, -9683, 452 };
+            if (github[2] == 352)
+            {
+                Console.WriteLine("Nice work");
+            }
+            else if(github[0] == 12)
+            {
+                Console.WriteLine("easy work");
+            }
+            else
+                Console.WriteLine("Go home!");
+
             Console.ReadLine();
         }
     }
